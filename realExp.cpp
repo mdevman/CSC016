f688d6c37c03f3d0b459677cda6fe40c6844f410
mdevman <mdevli1@pride.hofstra.edu>
2018-02-05 19:52:57 -0500

    end of class 2018-02-05

---
+++ realExp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+
+  for(double d = 0.0; d <= 5.0; d += 0.1)
+  {
+    cout<<d<<endl;
+    
+  }
+  cout<<"The absolute value of -5 is "<<abs(-5)<<endl;
+}
