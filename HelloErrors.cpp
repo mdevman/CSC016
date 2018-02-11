51210631fb3b69b0e43904661450cc9ac1ade089
mdevman <mdevman@optonline>
2018-02-11 14:28:29 -0500

    commit from surface pro 20180211

---
+++ HelloErrors.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+const float MyConstantValue = 9.9;
+
+int testFunc1()
+{
+ return MyConstantValue * 2;
+}
+
+int testFunc2(int val)
+{
+ return val*testFunc1();
+}
+
+int testFunc3(int y)
+{
+ int x = 4;
+ return testFunc2(x)*testFunc2(y);
+}
+
+int main()
+{
+ cout << "!!!Hello Errors!!!" << endl;
+ int result = testFunc3(10);
+ cout << "Result="<<result<<endl;
+ return 0;
+}
