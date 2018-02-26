28bc6a9d4a2facb30070b8ed4536303bcd1771c7
mdevman <mdevman@optonline>
2018-02-25 19:55:51 -0500

    after session at home 20180225

---
+++ sieve.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+In mathematics, the sieve of Eratosthenes is a simple, ancient algorithm for finding all prime numbers up to any given limit.
+
+It does so by iteratively marking as composite (i.e., not prime) the multiples of each prime, starting with the first prime number, 2. The multiples of a given prime are generated as a sequence of numbers starting from that prime, with constant difference between them that is equal to that prime.[1] This is the sieve's key distinction from using trial division to sequentially test each candidate number for divisibility by each prime
+*/
+
+int main()
+{
+	//Get the range of numbers from user
+	//Load those numbers into an array
+	//Loop through the numbers in the array
+	//Determine if the current number is a prime number
+	 //Only have to check up the current number's square root
+	//Loop throught he rest of the array and discard any numbers that are divisible by this number
+}
